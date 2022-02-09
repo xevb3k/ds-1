bb3c624c22ce41c65542a94b1df7c6a83c5e763e
xevb3k <jorom2004@mail.ru>
2022-02-10 01:25:52 +0300

    add project files

---
+++ first.py
@@ -0,0 +1,23 @@
+from asyncio.constants import SENDFILE_FALLBACK_READBUFFER_SIZE
+
+import numpy as np
+
+
+
+
+number = np.random.randint(1, 101)
+
+
+count = 0
+while True:
+    count +=1 
+    predict_number = int(input("enter number 1..100: "))
+    
+    if predict_number > number:
+        print("Число должно быть меньше!")
+    elif predict_number < number:
+        print("Число должно быть больше!")
+    else: 
+        print(f"Вы угадали число! {number}, за {count} попыток")
+        break
+
